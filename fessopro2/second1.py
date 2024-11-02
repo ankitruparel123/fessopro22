c185680dc83ba053562624ce570c9898148d1ec5
ankitruparel123 <ankitruparel123@gmail.com>
2024-11-02 11:06:45 +0530

    first commit

---
+++ fessopro2/second1.py
@@ -0,0 +1,9 @@
+print('hello')
+print('world')
+print('this is my world')
+#choose odd even given number
+number=13
+if number % 2 == 0 : 
+    print("number=even")
+else :
+    print("number is odd")
